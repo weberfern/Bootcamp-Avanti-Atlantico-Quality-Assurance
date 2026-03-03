4bbbaeca129fa597a5965194c1f6aa9d85654d23
Nycole Rodrigues <nycolec.rodrigues@gmail.com>
2026-03-03 19:12:36 -0300

    adicionei a automação de busca por um filme inexistente no arquivo busca.cy.js

--- cypress/e2e/busca.cy.js
+++ cypress/e2e/busca.cy.js
@@ -14,3 +14,17 @@ describe('Busca no TMDB', () => {
   })
 
 })
+
+describe('busca filme no TMDB', () => {
+  it('Buscar um filme inexistente', () => {
+    cy.visit('https://www.themoviedb.org/')
+    // localizar campo de busca
+    cy.get('#inner_search_v4').should('be.visible')
+    // digitar termo e enviar
+    cy.get('#inner_search_v4').type('filme inexistente{enter}')
+    // validar que resultado aparece
+    cy.contains('Não foram encontrados filmes que correspondam aos seus critérios de busca.').should('be.visible')
+
+  })
+
+})
